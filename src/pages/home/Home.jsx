81ef53d8f39c7d6cdaa6d854af90e56cb58987f9
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-21 02:55:14 +0300

    refactor: Use h2 header for heading instead of h1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   return (
     <main className={container}>
       <div className={textContainer}>
-        <h1 className={heading}>Welcome to Fake Store!</h1>
+        <h2 className={heading}>Welcome to Fake Store!</h2>
         <p className={text}>
           Fake Store is your one-stop destination for fashion-forward clothing
           and exquisite jewelry for both men and women. Step into a world of
