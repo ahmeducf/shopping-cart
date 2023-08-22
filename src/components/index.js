ed4827f5be47d9047090de7548d3434eed9b764b
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-23 01:13:03 +0300

    chore: Create DeleteButton component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ItemCount } from './item-count';
 export { default as ItemPrice } from './item-price';
 export { default as AddToCartButton } from './add-to-cart-button';
+export { default as DeleteButton } from './delete-button';
