e74b3aec16d27368499b9f454ae97566b691d910
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-25 02:30:11 +0300

    chore: Handle button click event

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as ItemCount } from './item-count';
 export { default as ItemPrice } from './item-price';
 export { default as AddToCartButton } from './add-to-cart-button';
 export { default as DeleteButton } from './delete-button';
+export { default as AddedToCart } from './added-to-cart';
