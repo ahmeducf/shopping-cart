f8e24229310102bbae649b31b6b8466745681fc3
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-25 02:28:27 +0300

    refactor: Incorprate CartContext and refactor hooks mock

--- src/components/__tests__/ItemCount.test.jsx
+++ src/components/__tests__/ItemCount.test.jsx
@@ -1,16 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
-import { ItemCount } from 'components';
 import userEvent from '@testing-library/user-event';
+import ProductItem from 'features/products/products-list/product-item/ProductItem';
+import ProductsProvider from 'contexts/ProductsContext';
+import CartProvider from 'contexts/CartContext';
+import { BrowserRouter } from 'react-router-dom';
+
+const product = {
+  id: 1,
+  title: 'Test product',
+  price: 100,
+  image: 'https://via.placeholder.com/150',
+};
 
 describe('ItemCount', () => {
   it('should have a valid initial state', () => {
-    render(<ItemCount />);
+    render(
+      <BrowserRouter>
+        <ProductsProvider>
+          <CartProvider>
+            <ProductItem product={product} />
+          </CartProvider>
+        </ProductsProvider>
+      </BrowserRouter>,
+    );
     expect(screen.getByRole('spinbutton', { name: 'Count' })).toHaveValue(1);
   });
 
   it('should increase the count when the increase button is clicked', async () => {
-    render(<ItemCount />);
+    render(
+      <BrowserRouter>
+        <ProductsProvider>
+          <CartProvider>
+            <ProductItem product={product} />
+          </CartProvider>
+        </ProductsProvider>
+      </BrowserRouter>,
+    );
 
     const user = userEvent.setup();
 
@@ -20,7 +46,15 @@ describe('ItemCount', () => {
   });
 
   it('should decrease the count when the decrease button is clicked', async () => {
-    render(<ItemCount />);
+    render(
+      <BrowserRouter>
+        <ProductsProvider>
+          <CartProvider>
+            <ProductItem product={product} />
+          </CartProvider>
+        </ProductsProvider>
+      </BrowserRouter>,
+    );
 
     const user = userEvent.setup();
 
