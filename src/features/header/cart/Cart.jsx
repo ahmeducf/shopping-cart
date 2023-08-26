8253e4d7142727e3fed2c20bb0709d2f42c68a34
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-26 19:21:39 +0300

    feat: Add cart length badge

--- src/features/header/cart/Cart.jsx
+++ src/features/header/cart/Cart.jsx
@@ -1,13 +1,20 @@
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-import { cart, iconContainer } from './Cart.module.css';
+import { iconContainer, cartLengthLabel } from './Cart.module.css';
+import { useCart } from 'hooks';
 
 function Cart() {
+  const { cart } = useCart();
+  const cartLength = cart.length;
+
   return (
     <Link to="/checkout" aria-label="Checkout" className={cart}>
       <div className={iconContainer}>
         <FontAwesomeIcon icon={faShoppingCart} />
+        {cartLength > 0 && (
+          <span className={cartLengthLabel}>{cartLength}</span>
+        )}
       </div>
     </Link>
   );
