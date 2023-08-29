3a1eeee44791c2b3d4b77102fb160deb6ed507f8
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-08-29 03:27:17 +0300

    feat: Add mobile navigation menu for products page

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -7,17 +7,23 @@ import {
   hidden,
   productsSection,
   heading,
+  toggleSidebarWrapper,
+  toggleSidebarButton,
+  active,
 } from './Products.module.css';
 import { useState } from 'react';
 import { useFetchProducts } from 'hooks';
 import { Error as ErrorPage } from 'pages';
 import { LoadingSpinner } from 'components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Products() {
   const { products: productsData, isLoading, error } = useFetchProducts();
   const [selectedCategory, setSelectedCategory] = useState('all');
   const pageHeading =
     selectedCategory.slice(0, 1).toUpperCase() + selectedCategory.slice(1);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   if (error) {
     return <ErrorPage error={error} />;
@@ -53,6 +59,33 @@ function Products() {
           />
         </section>
       </div>
+      <div
+        className={`${toggleSidebarWrapper} ${isSidebarOpen ? active : ''}`}
+      ></div>
+      <button
+        className={toggleSidebarButton}
+        aria-label="Toggle sidebar"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      >
+        <FontAwesomeIcon icon={isSidebarOpen ? faXmark : faBars} />
+      </button>
+      <aside
+        className={`${filtersSidebar} ${isSidebarOpen ? '' : hidden}`}
+        aria-label="Filters"
+        role="listbox"
+        aria-hidden={!isSidebarOpen}
+        aria-expanded={isSidebarOpen}
+      >
+        <h3 className={hidden}>Filters</h3>
+        <div aria-label="Categories" role="list">
+          <h4 className={filterHeading}>Categories</h4>
+          <CategoryList
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+            setIsSidebarOpen={setIsSidebarOpen}
+          />
+        </div>
+      </aside>
     </main>
   );
 }
